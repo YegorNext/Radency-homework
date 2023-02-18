3e2fe675f0efba781036f59634fa886e0234a9a6
YegorNext <yegor.d2015@gmail.com>
2023-02-18 22:41:02 +0200

    v0.1-validating

--- Task1/ConsoleApp1/Program.cs
+++ Task1/ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
 {
@@ -17,6 +18,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /// Stage 1 - Reading file names
             Config jsonData = JsonSerializer.Deserialize<Config>(System.IO.File.ReadAllText("D:\\Student\\Radency\\Task1\\ConsoleApp1\\Task1\\ConsoleApp1\\config.json")); 
             
             var files_name = Directory.GetFiles(jsonData.path);
@@ -25,6 +27,16 @@ namespace ConsoleApp1
                 FileInfo fileinf = new FileInfo(file);
                 if (fileinf.Extension == ".txt" || fileinf.Extension == ".csv") Console.WriteLine(fileinf.Name + fileinf.Extension);
 
+            }
+
+
+            //// Stage 2 - Validating
+            StreamReader reader = new StreamReader("D:\\Student\\Radency\\Task1\\ConsoleApp1\\Task1\\Data\\t1_c1.txt");
+            string line, pattern = @"[a-z]+,[a-z]+,'[a-z]+,\s[a-z]+\s\d+,\s\d+',\d+[.]\d+,[0-9]{4}-[0-9]{2}-[0-9]{2},[0-9]+,[a-z]+";
+            
+            while((line = reader.ReadLine()) != null)
+             {
+                Console.WriteLine(Regex.IsMatch(line, pattern, RegexOptions.IgnoreCase));
             }
             Console.ReadLine();
         }
