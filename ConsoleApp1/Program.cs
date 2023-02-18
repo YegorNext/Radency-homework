346187398d5aa80bfc7d4f0ea817435e53f6005e
YegorNext <yegor.d2015@gmail.com>
2023-02-18 17:40:49 +0200

    v0.1-filereading

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string FilesPath = "D:\\Student\\Radency\\Task1\\ConsoleApp1\\ConsoleApp1\\Data";
+            var files_name = Directory.GetFiles(FilesPath);
+            foreach (var file in files_name)
+            {
+                FileInfo fileinf = new FileInfo(file);
+                if (fileinf.Extension == ".txt" || fileinf.Extension == ".csv") Console.WriteLine(file);
+
+            }
+            Console.ReadLine();
         }
     }
 }
