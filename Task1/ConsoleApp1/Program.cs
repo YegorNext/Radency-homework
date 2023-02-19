0d6b91fec8db12b456d6960ee353477ed7bbcd19
YegorNext <yegor.d2015@gmail.com>
2023-02-19 18:43:28 +0200

    v0-1-database && query

--- Task1/ConsoleApp1/Program.cs
+++ Task1/ConsoleApp1/Program.cs
@@ -8,6 +8,9 @@ using System.Text.Json;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using System.Web;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 namespace ConsoleApp1
 {
@@ -16,16 +19,19 @@ namespace ConsoleApp1
         public string path { get; set; }
 
     }
+
     public class TransactionData
     {
         public string city { get; set; }
-        public IList<Services> services{ get; set; }
+        public IEnumerable<Services> services { get; set; }
+        public decimal total { get; set; }
     }
     public class Services
     {
         public string name { get; set; }
-        public IList<Payers> payers { get; set; }
+        public IEnumerable<Payers> payers{ get; set; }
 
+        public decimal total { get; set; }
     }
     public class Payers
     {
@@ -34,26 +40,7 @@ namespace ConsoleApp1
         public string date { get; set; }
         public long account_number { get; set; }
     }
-    public class FileProcessTransfer
-    {
-        private string line;
-        private string[] package;
 
-        public string Line
-        {
-            get { return line; }
-        }
-        FileProcessTransfer(string line)
-        {
-            this.line = line;
-            package = new string[5];
-        }
-
-        private void sepData()
-        {
-
-        }
-    }
     internal class Program
     {
         static void Main(string[] args)
@@ -81,19 +68,56 @@ namespace ConsoleApp1
             reader.Close();
 
             ///Stage 3 - Transofrmation
-            var data = new TransactionData
-            {
-                city = "Dnipro",
-                services = new List<Services>()
-                {
-                    new Services() {name = "Gaz", payers = new List<Payers>(){ 
-                        new Payers(){name = "John", payment = Convert.ToDecimal(535.5), date = "353", account_number = 2352562}
-                    } }
-                }
-            };
 
+
+            string[] str = new string[6]; string patt = @"'[a-z]+,\s[a-z]+\s[0-9]+,\s[0-9]+'";
+            str[0] = "John,Doe,'Lviv, Kleparivska 35, 4',500.0,2022-27-01,1234567,Water";
+            str[1] = "Mark,Doe,'Lviv, Kleparivska 35, 4',1250.35,2022-27-01,1234567,Gaz";
+            str[2] = "Charlz,Doe,'Lviv, Kleparivska 35, 4',1250.35,2022-27-01,1234567,Gaz";
+            str[3] = "Carl,Doe,'Pavlograd, Kleparivska 35, 4',1250.35,2022-27-01,1234567,Gaz";
+            str[4] = "Sem,Doe,'Pavlograd, Kleparivska 35, 4',600.35,2022-27-01,1234567,Gaz";
+            str[5] = "Shara,Doe,'Pavlograd, Kleparivska 35, 4',250.35,2022-27-01,1234567,Water";
+
+
+            var database = from Line in str
+                           let toSplit = Regex.Match(Line, patt, RegexOptions.IgnoreCase).Value
+                           let SplitLine = Line.Replace(toSplit + ',', "").Split(',')
+                           let city = toSplit.Replace("\'", "").Split(',')[0]
+                           select new
+                           {
+                               city,
+                               service_name = SplitLine[5],
+                               payer = SplitLine[0] + "" + SplitLine[1],
+                               payment = decimal.Parse(SplitLine[2].Replace(".", ",")),
+                               date = SplitLine[3],
+                               account_num = long.Parse(SplitLine[4])
+                           };
+
+            var query =
+                from values in database
+                group values by values.city into g
+
+                select new TransactionData
+                {
+                    city = g.Key,
+                    services = (from srvc in database
+                                where srvc.city == g.Key 
+                                group srvc by srvc.service_name into h
+                                select new Services
+                                {
+                                    name = h.Key,
+                                    payers = (from pymnt in database 
+                                    where pymnt.service_name == h.First().service_name && pymnt.city == h.First().city
+                                    select new Payers{ name = pymnt.payer, payment = pymnt.payment, date = pymnt.date, account_number = pymnt.account_num }  
+                                    ),
+                                    total = h.Sum(x => x.payment)
+                                }
+                               ),
+                    total = g.Sum(x=> x.payment)
+                };
+            
             var options = new JsonSerializerOptions { WriteIndented = true };
-            string jsonString = JsonSerializer.Serialize(data, options);
+            string jsonString = JsonSerializer.Serialize(query, options);
             Console.WriteLine(jsonString);
 
             Console.ReadLine();
